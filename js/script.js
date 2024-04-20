277afdb3f94d263d06a897b54891dcfd99a0cbc5
Ivan-Ndahiro <ivanndahiro02@gmail.com>
2024-04-20 15:06:39 +0100

    Implemented working timer functionality and CSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let step = document.getElementsByClassName('step');
-let previous = document.getElementById('previous');
-let next = document.getElementById('next');
-let submit = document.getElementById('submit');
+let previousButton = document.getElementById('previous');
+let nextButton = document.getElementById('next');
+let submitButton = document.getElementById('submit');
 let form = document.getElementsByTagName('form')[0];
 let transition = document.getElementById('transition');
 let body_element = document.querySelector('body');
@@ -16,30 +16,63 @@ form.onsubmit = () => {
 step[current_step].classList.add('d-block');
 
 if (current_step == 0) {
-    previous.classList.add('d-none');
-    submit.classList.add('d-none');
-    next.classList.add('d-inline-block');
+    previousButton.classList.add('d-none');
+    submitButton.classList.add('d-none');
+    nextButton.classList.add('d-inline-block');
 }
 
 const progress = (value) => {
     document.getElementsByClassName('progress-bar')[0].style.width = `${value}%`;
 }
 
-next.addEventListener('click', () => {
+//Event calls to correctly load pages when pressed the previous, Next and Submit Button
+previousButton.addEventListener('click', previous);
+nextButton.addEventListener('click', next);
+submitButton.addEventListener('click', submit);
+
+//Function to load pages and calculate Progress bar for previous button
+function previous(){
+    if (current_step > 0) {
+        current_step--;
+        let previous_step = current_step + 1;
+        previousButton.classList.add('d-none');
+        previousButton.classList.add('d-inline-block');
+        step[current_step].classList.remove('d-none');
+        step[current_step].classList.add('d-block')
+        step[previous_step].classList.remove('d-block');
+        step[previous_step].classList.add('d-none');
+        if (current_step < stepCount) {
+            submitButton.classList.remove('d-inline-block');
+            submitButton.classList.add('d-none');
+            nextButton.classList.remove('d-none');
+            nextButton.classList.add('d-inline-block');
+            previousButton.classList.remove('d-none');
+            previousButton.classList.add('d-inline-block');
+        }
+    }
+    if (current_step == 0) {
+        previousButton.classList.remove('d-inline-block');
+        previousButton.classList.add('d-none');
+    }
+    progress((100 / stepCount) * current_step);
+}
+
+//Function to load pages and calculate Progress bar on press of Next Button
+function next(){
     current_step++;
     let previous_step = current_step - 1;
     if ((current_step > 0) && (current_step <= stepCount)) {
-        previous.classList.remove('d-none');
-        previous.classList.add('d-inline-block');
+        previousButton.classList.remove('d-none');
+        previousButton.classList.add('d-inline-block');
         step[current_step].classList.remove('d-none');
         step[current_step].classList.add('d-block');
         step[previous_step].classList.remove('d-block');
         step[previous_step].classList.add('d-none');
         if (current_step == stepCount) {
-            submit.classList.remove('d-none');
-            submit.classList.add('d-inline-block');
-            next.classList.remove('d-inline-block');
-            next.classList.add('d-none');
+            submitButton.classList.remove('d-none');
+            submitButton.classList.add('d-inline-block');
+            nextButton.classList.remove('d-inline-block');
+            nextButton.classList.add('d-none');
         }
     } else {
         if (current_step > stepCount) {
@@ -49,54 +82,55 @@ next.addEventListener('click', () => {
         }
     }
     progress((100 / stepCount) * current_step);
-});
- 
-previous.addEventListener('click', () => {
-    if (current_step > 0) {
-        current_step--;
-        let previous_step = current_step + 1;
-        previous.classList.add('d-none');
-        previous.classList.add('d-inline-block');
-        step[current_step].classList.remove('d-none');
-        step[current_step].classList.add('d-block')
-        step[previous_step].classList.remove('d-block');
-        step[previous_step].classList.add('d-none');
-        if (current_step < stepCount) {
-            submit.classList.remove('d-inline-block');
-            submit.classList.add('d-none');
-            next.classList.remove('d-none');
-            next.classList.add('d-inline-block');
-            previous.classList.remove('d-none');
-            previous.classList.add('d-inline-block');
-        }
-    }
-    if (current_step == 0) {
-        previous.classList.remove('d-inline-block');
-        previous.classList.add('d-none');
-    }
-    progress((100 / stepCount) * current_step);
-});
- 
-submit.addEventListener('click', () => {
-    calculate();
-    transition.classList.add('d-block');
-    const timer = ms => new Promise(res => setTimeout(res, ms));
-    timer(1000)
-        .then(() => {
-            body_element.classList.add('loaded');
-        }).then(() => {
-            step[stepCount].classList.remove('d-block');
-            step[stepCount].classList.add('d-none');
-            previous.classList.remove('d-inline-block');
-            previous.classList.add('d-none');
-            submit.classList.remove('d-inline-block');
-            submit.classList.add('d-none');
-            complete.classList.remove('d-none');
-            complete.classList.add('d-block');
-        })
- 
-});
+}
 
+//Function to load pages on press of Previous Button
+function submit(){
+    calculate();
+    for(var i=0;i<31;i++){
+        step[i].classList.remove('d-block');
+        step[i].classList.add('d-none');
+    }
+    submitButton.classList.remove('d-none');
+    submitButton.classList.add('d-inline-block');
+    nextButton.classList.remove('d-inline-block');
+    nextButton.classList.add('d-none');
+    body_element.classList.add('loaded');
+    step[stepCount].classList.remove('d-block');
+    step[stepCount].classList.add('d-none');
+    previousButton.classList.remove('d-inline-block');
+    previousButton.classList.add('d-none');
+    submitButton.classList.remove('d-inline-block');
+    submitButton.classList.add('d-none');
+    complete.classList.remove('d-none');
+    complete.classList.add('d-block');
+    console.log("Submit done!")
+}
+
+//Timer function calls Submit page 15 minutes on load and refreshes the countdown every second
+function timer(){
+    var time = 900000;
+    setTimeout(submit, time);
+    var countdown = setInterval(() => {
+        duration = time - 1000;
+        var m = Math.floor((duration % (1000*60*60))/(1000*60));
+        var s = Math.floor((duration % (1000 * 60))/1000);
+        if (s < 10){
+            s = "0" + s;
+            document.getElementById("timer").innerHTML = m + ":" + s;
+        } else{
+            document.getElementById("timer").innerHTML = m + ":" + s;
+        }
+        if (duration <= 0){
+            clearInterval(countdown);
+            document.getElementById("timer").innerHTML = "Done!"
+            submit();
+        }
+        time-=1000;
+    }, 1000);
+}
+
+//IQ function to calculate the IQ Score based on the result
 function iqScore(value){
     var mean = 15;
     var sd = 5;
@@ -106,6 +140,7 @@ function iqScore(value){
     return iqScore;
 }
 
+//Calculate function to tally up the correct scores and print out the correct value
 function calculate(){
     var total=0;
     var q1=document.getElementsByName('Q1');
@@ -319,6 +354,6 @@ function calculate(){
         }
     });
     document.getElementById("answerTotal").innerHTML = total;
-    var iq = iqScore(total);
-    document.getElementById("iqTotal").innerHTML = iq;
+    /*var iq = iqScore(total);
+    document.getElementById("iqTotal").innerHTML = iq;*/
 }
